c59c780766c2a6b6d288e2eaf0d5857d5d3b49ee
Florian Bailly <florian.bailly@gmx.com>
2017-03-16 15:41:45 +0100

    Added: entities

---
+++ formGenerator/src/FormGenerator/FormBundle/Entity/Question.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace FormGenerator\FormBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Question
+ *
+ * @ORM\Table(name="question")
+ * @ORM\Entity(repositoryClass="FormGenerator\FormBundle\Repository\QuestionRepository")
+ */
+class Question
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="question", type="string", length=255)
+     */
+    private $question;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="topic_id", type="integer")
+     */
+    private $topicId;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="is_multiple", type="boolean")
+     */
+    private $isMultiple;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set question
+     *
+     * @param string $question
+     *
+     * @return Question
+     */
+    public function setQuestion($question)
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    /**
+     * Get question
+     *
+     * @return string
+     */
+    public function getQuestion()
+    {
+        return $this->question;
+    }
+
+    /**
+     * Set topicId
+     *
+     * @param integer $topicId
+     *
+     * @return Question
+     */
+    public function setTopicId($topicId)
+    {
+        $this->topicId = $topicId;
+
+        return $this;
+    }
+
+    /**
+     * Get topicId
+     *
+     * @return int
+     */
+    public function getTopicId()
+    {
+        return $this->topicId;
+    }
+
+    /**
+     * Set isMultiple
+     *
+     * @param boolean $isMultiple
+     *
+     * @return Question
+     */
+    public function setIsMultiple($isMultiple)
+    {
+        $this->isMultiple = $isMultiple;
+
+        return $this;
+    }
+
+    /**
+     * Get isMultiple
+     *
+     * @return bool
+     */
+    public function getIsMultiple()
+    {
+        return $this->isMultiple;
+    }
+}
+
